04a02fa30078c704cff869e821cdae725f04744c
Abril del Burgo <pildelburgo@gmail.com>
2018-05-22 21:19:42 -0300

    ejercicio 5 y bonus

--- pinkyycerebro.hs
+++ pinkyycerebro.hs
@@ -22,9 +22,9 @@ agregarHabilidad :: String -> Transformacion
 agregarHabilidad unaHabilidad unAnimal = unAnimal { habilidades = ((:) unaHabilidad.habilidades) unAnimal }
 
 -- EJERCICIO 3
-type Propiedad = Animal -> Bool
+type CriterioDeExito = Animal -> Bool
 
-antropomorfico :: Propiedad
+antropomorfico :: CriterioDeExito
 antropomorfico unAnimal = (habilidadDeHablar.habilidades) unAnimal && (coeficienteMayorA60.coeficiente) unAnimal
 
 habilidadDeHablar :: Habilidades -> Bool
@@ -33,7 +33,7 @@ habilidadDeHablar = elem "hablar"
 coeficienteMayorA60 :: Int -> Bool
 coeficienteMayorA60 = (>60)
 
-noTanCuerdo :: Propiedad
+noTanCuerdo :: CriterioDeExito
 noTanCuerdo = esMayorA2.habilidadesPinkiescas.habilidades
 
 habilidadesPinkiescas :: Habilidades -> Habilidades
@@ -43,9 +43,9 @@ esMayorA2 :: Habilidades -> Bool
 esMayorA2 = (>2).length
 
 -- EJERCICIO 4
-data Experimento = Experimento { transformaciones :: [Transformacion] , criterioDeExito :: Propiedad }
+data Experimento = Experimento { transformaciones :: [Transformacion] , criterioDeExito :: CriterioDeExito }
 
-experimentoExitoso :: Experimento -> Propiedad
+experimentoExitoso :: Experimento -> CriterioDeExito
 experimentoExitoso unExperimento unAnimal = ((criterioDeExito unExperimento).(animalTransformado unAnimal.transformaciones)) unExperimento
 
 animalTransformado:: Animal -> [Transformacion] -> Animal
@@ -61,22 +61,31 @@ Como criterio de éxito, ver si quedó antropomórfico"
 -} 
 
 -- EJERCICIO 5
-reporte1::
-reporte1 listaAnimales listaHabilidades listaTransformaciones = filtradoDeCapacidades.(animalesTransformados listaTransformaciones listaAnimales)
+type Funcion = Int -> Bool
+
+reporte1 :: [Habilidades] -> [Transformacion] -> [Animal] -> [Int]
+reporte1 listaHabilidades listaTransformaciones = map coeficiente.(nuevosAnimales listaHabilidades listaTransformaciones funcion)
+ where funcion = (>0)
+
+nuevosAnimales :: [Habilidades] -> [Transformacion] -> Funcion ->[Animal] -> [Animal]
+nuevosAnimales listaHabilidades listaTransformaciones funcion = filtradoDeAnimales listaHabilidades funcion .(animalesTransformados listaTransformaciones)
 
 animalesTransformados :: [Transformacion] -> [Animal] -> [Animal]
-animalesTransformados listaTransformaciones listaAnimales = map (/animal -> animalTransformado animal listaTransformaciones)
+animalesTransformados listaTransformaciones = map (\animal -> animalTransformado animal listaTransformaciones)
 
-filtradoDeCapacidades ::
-filtradoDeCapacidades listaAnimales = filter (any estaEnListaCapacidades.capacidades) listaAnimales
+filtradoDeAnimales :: [Habilidades] -> Funcion -> [Animal] -> [Animal]
+filtradoDeAnimales listaHabilidades funcion = filter ((existeInterseccion funcion listaHabilidades).habilidades)
 
+existeInterseccion :: Funcion -> [Habilidades] -> [Habilidades] -> Bool
+existeInterseccion funcion listaHabilidades = funcion.length.intersect listaHabilidades
 
+reporte2 :: [Habilidades] -> [Transformacion] -> [Animal] -> [String]
+reporte2 listaHabilidades listaTransformaciones = map especie.(nuevosAnimales listaHabilidades listaTransformaciones funcion)
+ where funcion = (==length listaHabilidades)
 
-
-
-
-
-
+reporte3 :: [Habilidades] -> [Transformacion] -> [Animal] -> [Int]
+reporte3 listaHabilidades listaTransformaciones = map (length.habilidades).(nuevosAnimales listaHabilidades listaTransformaciones funcion)
+ where funcion = (==0)
 
 -- EJERCICIO 6
 {- 
@@ -86,6 +95,7 @@ Dar ejemplos de experimentos que se puedan realizar y que no, si los hubiera. Ju
 -}
 
 -- BONUS
+-- EJERCICIO A
 pinkiesco :: String -> Bool
 pinkiesco unaHabilidad = empiezaConHacer unaHabilidad && palabraPinkiesca unaHabilidad
 
@@ -102,3 +112,16 @@ tiene4Letras = (<=4).length
 tieneUnaVocal :: String -> Bool
 tieneUnaVocal = (>=1).length.filter esVowel 
  where esVowel letra = elem letra "AaEeIiOoUu"
+
+-- EJERCICIO B
+generarTodasLasPalabrasPinkiescas = (filtrarPinkiescas.agregarHacer.generateWordsUpTo) 4
+ 
+agregarHacer :: [String] -> [String]
+agregarHacer = map ("hacer " ++) 
+
+filtrarPinkiescas :: [String] -> [String]
+filtrarPinkiescas = filter pinkiesco 
+
+-- EJERCICIO C
+generateWordsUpTo :: Int -> [String]
+generateWordsUpTo = (concat.map generateWords) [1,2,3,4]
